977cf3aae0395ad46ec1630c11e2c359607ae18c
Mephrine <dthwrld@gmail.com>
2021-07-27 19:50:07 +0900

    Feat: Auth 관리용 클래스 추가

--- TodoRIBs/Utils/AuthManager.swift
+++ TodoRIBs/Utils/AuthManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Firebase
+import RxSwift
+
+protocol Authable: AnyObject {
+  static func login(withEmail email: String, password: String) -> Observable<AuthDataResult>
+}
+
+final class AuthManager: Authable {
+  
+}
+
+extension AuthManager {
+  public static func login(withEmail email: String, password: String) -> Observable<AuthDataResult> {
+    return Observable<AuthDataResult>.create { observer in
+      Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+        if let error = error {
+          observer.onError(error)
+        }
+        
+        if let result = result {
+          observer.on(.next(result))
+          observer.on(.completed)
+        }
+      }
+      return Disposables.create()
+    }
+  }
+}
