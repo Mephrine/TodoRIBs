f519f18c74a97e45a7f41361dad0d57516b905f0
Mephrine <dthwrld@gmail.com>
2021-07-27 19:50:30 +0900

    TODO: Auth 클래스 Mock을 만들어보자.

--- TodoRIBsTests/Utils/AuthMangerMocks.swift
+++ TodoRIBsTests/Utils/AuthMangerMocks.swift
@@ -5,4 +5,31 @@
 //  Created by Mephrine on 2021/07/14.
 //
 
-import Foundation
+@testable import TodoRIBs
+import RxSwift
+import Firebase
+
+//enum TestAuthError: Error {
+//  case authError
+//}
+
+//final class AuthManagerMocks: Authable {
+//  static func login(withEmail email: String, password: String) -> Observable<AuthDataResult> {
+//    return Observable.create { observer in
+//      if email.isEmpty || password.isEmpty {
+//        observer.on(.error(TestAuthError.authError))
+//      }
+//      let test = AuthDataResult(test: true)
+////      observer.on(.next()
+//      return Disposables.create()
+//    }
+//  }
+//}
+//
+//protocol TestModel {}
+//
+//extension AuthDataResult: TestModel {
+//  convenience init(test: Bool) {
+//    return AuthDataResult()
+//  }
+//}
